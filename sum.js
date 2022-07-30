30096f75e77d86923dcb9dc3bc2fe988fb646134
Yusa <anthya1104@gmail.co>
2022-07-30 11:40:17 +0800

    add sum function

---
+++ sum.js
@@ -0,0 +1,16 @@
+//sum.js
+
+function sum(n){
+    let numberSum = 0; 
+    for(i=0; i<=n; i++){
+        
+        numberSum += i;
+       
+    }
+    console.log(numberSum);
+
+}
+
+sum(1);
+sum(3);
+sum(10);
