5887e11c118b33e21eb42f33253ea31014015fec
Yusa <anthya1104@gmail.co>
2022-07-30 11:33:14 +0800

    hello.js add

---
+++ hello.js
@@ -0,0 +1,9 @@
+console.log('hello nodejs');
+
+// test 
+// let cats = ["red", "blue", "black"];
+// cats.push("white");
+// console.log(cats);
+
+// window是瀏覽器提供 所以node.js不能使用
+// window.location.href = 'http://www.google.com';
