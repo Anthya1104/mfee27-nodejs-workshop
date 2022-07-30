f269db5b70805047b030898ff10ea74569965e47
Yusa <anthya1104@gmail.co>
2022-07-30 13:41:37 +0800

    prettier wait to fix & function test add

--- sum.js
+++ sum.js
@@ -1,53 +1,56 @@
 //sum.js
 
 function sum(n) {
-    let numberSum = 0; 
-    for(i = 0; i <= n; i++){
-        
-        numberSum += i;
-       
-    }
-    return numberSum;
-
+  let numberSum = 0;
+  for (i = 0; i <= n; i++) {
+    numberSum += i;
+  }
+  return numberSum;
 }
 
-console.log(sum(1));
-console.log(sum(3));
-console.log(sum(10));
+// console.log(sum(1));
+// console.log(sum(3));
+// console.log(sum(10));
 
 // solution 2
-function sum2(n){
-    return ((n+1)*n)/2;
+function sum2(n) {
+  return ((n + 1) * n) / 2;
 }
 
-console.log(sum2(1));
-console.log(sum2(3));
-console.log(sum2(10));
+// console.log(sum2(1));
+// console.log(sum2(3));
+// console.log(sum2(10));
 
 //solution 3 遞迴
-function sum3(n){
-    if(n === 1){
-        return n;
-    }
-    return sum3(n-1)+n;
-    //重複呼叫sum3
-    //if n = 5 -> sum3(4)+5 -> sum3(3)+4+5 -> ..... -> 1+2+3+4+5
+function sum3(n) {
+  if (n === 1) {
+    return n;
+  }
+  return sum3(n - 1) + n;
+  //重複呼叫sum3
+  //if n = 5 -> sum3(4)+5 -> sum3(3)+4+5 -> ..... -> 1+2+3+4+5
 }
-console.log(sum3(1));
-console.log(sum3(3));
-console.log(sum3(10));
+// console.log(sum3(1));
+// console.log(sum3(3));
+// console.log(sum3(10));
 
+//function 4 reduce (待解))
 
-// test:
-// function sumType2(n){
-//     let n;
-//     if( n == 1 ){
-//         console.log(1);
-//     }else{
-//         console.log(sum(n-1)+n)
-//     }
-// }
+//測試效能 測數字很大時
+console.time('SUM1');
+for (let i = 1; i <= 1000; i++) {
+    sum(10000);
+}
+console.timeEnd('SUM1');
 
-// sumType2(1);
-// sumType2(3);
-// sumType2(10);
+console.time('SUM2');
+for (let i = 1; i <= 10000; i++) {
+    sum2(1000);
+}
+console.timeEnd('SUM2');
+
+console.time('SUM3');
+for (let i = 1; i <= 10000; i++) {
+    sum3(1000);
+}
+console.timeEnd('SUM3');
