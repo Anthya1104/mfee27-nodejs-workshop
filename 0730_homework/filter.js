a0e146a61f06893d03ef0795512c05222b1d0bfa
Yusa <anthya1104@gmail.co>
2022-08-04 13:19:06 +0800

    [feat] filter function modified

--- 0730_homework/filter.js
+++ 0730_homework/filter.js
@@ -18,8 +18,22 @@ let ary = [
 
 //濾出type 是A
 
+//array.filter
+let result0 = ary.filter((item) => item.type === 'A')
+
+
+console.log(result0)
+
 //for loop
 
 function aryFilter(ary) {
   let result = [];
+  for ( i = 0; i< ary.length; i++){
+    if ( ary[i].type === 'A'){
+      result.push(ary[i])
+    }
+  }
+  return result;
 }
+
+console.log(aryFilter(ary))
