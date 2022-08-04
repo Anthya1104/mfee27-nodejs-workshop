5656fa48d663d4d40a7b77bddf202ec212267c3c
Yusa <anthya1104@gmail.co>
2022-08-04 13:43:03 +0800

    [feat] map function WIP

---
+++ 0730_homework/map.js
@@ -0,0 +1,39 @@
+let kvArray = [{key: 1, value: 10},
+    {key: 2, value: 20},
+    {key: 3, value: 30}];
+
+//重新map一個陣列 讓key值變成key value值變成value
+// newAry = [
+//     { '1': 10}, { '2': 20 }, { '3': 30} 
+// ]    
+
+//array.map
+
+let reformattedArray = kvArray.map(function(obj) {
+let rObj = {};
+rObj[obj.key] = obj.value;
+return rObj;
+});
+
+console.log(reformattedArray)
+
+//for loop
+function aryMap(ary){
+    let result = []
+    let rObj = {}
+    for ( i = 0; i < ary.length; i++){
+        rObj[ary[i].key] = ary[i].value
+        
+        
+    }result.push(rObj)
+    return result
+}
+console.log(aryMap(kvArray))
+
+//待解問題:
+// 目前跑出的結果 kvArray.map :
+// [ { '1': 10 }, { '2': 20 }, { '3': 30 } ]
+
+// 用for loop跑出的是 :
+// [ { '1': 10, '2': 20, '3': 30 } ]
+// 在迴圈內所有object會被鋪平放在同一個array裡，如果想做到只在第i個子陣列裡放{ key[i] : value[i]} 該怎麼實現
