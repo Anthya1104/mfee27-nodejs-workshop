57d1e1fbc10e503a4efe087bc3959ebb7991656c
Yusa <anthya1104@gmail.co>
2022-08-11 13:56:04 +0800

    [feat]await_readfile upload

---
+++ 0806_HW/await_readfile.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+function fileReader(filePath, fileType) {
+  return new Promise((resolve, reject) => {
+    // reject('出錯啦讀不到');
+
+    resolve(
+      fs.readFile(filePath, fileType, (data, err) => {
+        if (err) {
+          return console.error(err);
+        }
+        console.log(data);
+      })
+    );
+  });
+}
+
+async function fileReading() {
+  try {
+    let file1Result = await fileReader('0806_HW/test.txt', 'utf8');
+    file1Result;
+    let file2Result = await fileReader('0806_HW/test2.txt', 'utf8');
+    file2Result;
+    let file3Result = await fileReader('0806_HW/test3.txt', 'utf8');
+    file3Result;
+  } catch (err) {
+    console.log(`出錯啦: ${err}`);
+  }
+}
+
+fileReading();
