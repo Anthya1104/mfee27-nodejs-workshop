cac61afb22efad50b8c6513d5e509c60ef633971
Yusa <anthya1104@gmail.co>
2022-08-12 13:27:31 +0800

    [fix] debug

--- promise/readfile.js
+++ promise/readfile.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-fs.readFile('test.txt', 'utf8', (err, data) => {
+fs.readFile('promise/test.txt', 'utf8', (err, data) => {
   if (err) {
     return console.error(err);
   }
