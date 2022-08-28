95a56358875402fd96554054c1f63604b4d48206
Yusa <anthya1104@gmail.co>
2022-08-28 11:58:08 +0800

    [feat]be page settings CPLT

--- stock-be/server.js
+++ stock-be/server.js
@@ -107,14 +107,46 @@ app.get('/api/1.0/stocks/:stockId', async (req, res, next) => {
   const stockId = req.params.stockId;
 
   //  去資料庫撈資料
-  let [data] = await pool.execute(`SELECT * FROM stock_prices WHERE stock_id=?`, [stockId]);
-  // let [data] = await pool.execute(`SELECT * FROM stock_prices WHERE stock_id=${stockId}`);
+  // 撈出全部資料
+  // let [Data] = await pool.execute(`SELECT * FROM stock_prices WHERE stock_id=?`, [stockId]);
+
+  // 分頁寫法
+  // 透過 query string 取得目前要第幾頁資料
+  // 如果沒有設定，預設要第一頁的資料
+  let page = req.query.page || 1;
+  // 每一頁五筆資料
+  let perPage = 6;
+
+  // 取得總筆數
+
+  let [total] = await pool.execute(`SELECT COUNT(*) AS total FROM stock_prices WHERE stock_id=?`, [stockId]);
+  total = total[0].total;
+
+  // 計算總頁數 Math.ceil
+  let lastPage = Math.ceil(total / perPage);
+
+  // 計算 offset
+  const offset = perPage * (page - 1);
+
+  // 根據 perPage 及 offset 去取得資料
+  // SELECT * FROM stock_prices WHERE stock_id ORDER BY date LIMIT 限制一次抓幾筆 OFFSET 跳過幾筆
+  let [data] = await pool.execute('SELECT * FROM stock_prices WHERE stock_id=? ORDER BY date LIMIT ? OFFSET ?', [stockId, perPage, offset]);
 
   //  把取得的資料回覆給前端
 
+  res.json({
+    pagination: {
+      total, // 總共有幾筆
+      perPage, // 一頁有幾筆
+      page, // 目前在第幾頁
+      lastPage, // 總頁數
+    },
+    data,
+  });
+
   // 開始寫中間前，先回 stockId 確定有成功包成API
   // res.json({ stockId });
-  res.json(data);
+  // res.json(data);
 });
 
 app.use((req, res, next) => {
