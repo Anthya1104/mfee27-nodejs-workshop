72e0d5cd4263fcf0d88f62fc69d0206e97de91cf
Yusa <anthya1104@gmail.co>
2022-09-03 09:07:17 +0800

    [add].env.example updated

--- stock-be/routers/auth.js
+++ stock-be/routers/auth.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 // 對單一 router 節點 使用
 router.post('/api/1.0/auth/register', express.json(), (req, res, next) => {
-  // TODO: 確認資料有無收到
+  // 確認資料有無收到
   console.log('register', req.body);
   // TODO: 檢查 email 有沒有重複
   // TODO: 如果有，回覆 400 跟錯誤訊息
